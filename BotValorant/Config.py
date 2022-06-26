510bec7d4f3d299900517090e7773d7cdb2db121
jean-michel.bernard@epitech.eu <jean-michel.bernard@epitech.eu>
2022-06-26 18:37:08 +0200

    Purge new branche

--- BotValorant/Config.py
+++
@@ -1,4 +0,0 @@
-from dotenv import dotenv_values
-
-# on récupère le contenu du fichier environement
-my_config = dotenv_values(".env")
--- BotValorant/GameApp/gameApp/PyInclude.py
+++
@@ -1,7 +0,0 @@
-#
-#   liste d'include à récupérer dans les autres fichier
-#
-from GameApp.gameApp.controllers.dataBaseController import dataBase
-from GameApp.gameApp.dataModel.playerModel import Player
-from GameApp.gameApp.Config.ConfigMod import ConfigMod
-from GameApp.gameApp.controllers.apiControllers.riotApiController import riotApi
