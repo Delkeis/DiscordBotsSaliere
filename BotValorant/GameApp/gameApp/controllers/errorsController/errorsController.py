3c2d88aa6b48f0f0f050c0c66b520cb7e9daf5ac
jean-michel.bernard@epitech.eu <jean-michel.bernard@epitech.eu>
2022-06-26 10:42:45 +0200

    Gestion des code erreur

---
+++ BotValorant/GameApp/gameApp/controllers/errorsController/errorsController.py
@@ -0,0 +1,15 @@
+import json
+# import de configMod pour récupérer le chmin des fichiers de config 
+from GameApp.gameApp.PyInclude import *
+
+class ErrorController:
+    def __init__(self, errorCode: int):
+        self.errorCode = errorCode
+        self.path = ".\\GameApp\\gameApp\\controllers\\errorsController\\errorCode.json"
+        with open(self.path, 'r') as f:
+            self.codeDd = json.load(f)
+        self.callErrorCode(errorCode)
+
+    def callErrorCode(self, errorCode: int):
+        
+        for i in self.codeDd.get('Error_Code'):
