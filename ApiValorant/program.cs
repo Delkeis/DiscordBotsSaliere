49e93cf6f2557f8457023fd575cb662d73832591
jean-michel Bernard <jean-michel.bernard@epitech.eu>
2022-11-17 11:10:10 +0100

    push base api

--- ApiValorant/program.cs
+++ ApiValorant/program.cs
@@ -1,5 +1,25 @@
-class ValorantApi{
-      void main(String[]){
-      	   Console.writeline("ceci est un test d'api");
-      }
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
