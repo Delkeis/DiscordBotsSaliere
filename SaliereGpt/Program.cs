080b8d2afec114a51a7d8cae4df92876f8f27322
delkeis <jean-michel.bernard@epitech.eu>
2023-01-04 20:05:41 +0100

    Add fonction basic bot C#

--- SaliereGpt/Program.cs
+++ SaliereGpt/Program.cs
@@ -1,71 +1,34 @@
-﻿using System;
-using System.Linq;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Interactivity;
+﻿using Discord;
+using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+using System;
+using System.Threading.Tasks;
 
 internal class Program
 {
-    /* This is the cancellation token we'll use to end the bot if needed(used for most async stuff). */
-    private CancellationTokenSource _cts { get; set; }
+    private IConfigurationRoot? _config;
+    private DiscordSocketClient _client;
 
-    /* We'll load the app config into this when we create it a little later. */
-    private IConfigurationRoot _config;
+    static async Task Main(string[] args)
+    {
+        OpenAIAPI api = new OpenAIAPI();
 
-    /* These are the discord library's main classes */
-    private DiscordClient _discord;
-    private CommandsNextModule _commands;
-    private InteractivityModule _interactivity;
-
-    /* Use the async main to create an instance of the class and await it(async main is only available in C# 7.1 onwards). */
-    static async Task Main(string[] args) => await new Program().InitBot(args);
-
+        await new Program().InitBot(args);
+    }
     async Task InitBot(string[] args)
     {
         try
         {
-            Console.WriteLine("[info] Welcome to my bot!");
-            _cts = new CancellationTokenSource(); 
-
-            // Load the config file(we'll create this shortly)
             Console.WriteLine("[info] Loading config file..");
             _config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("config.json", optional: false, reloadOnChange: true)
                 .Build();
+            _client = new DiscordSocketClient();
 
-            // Create the DSharpPlus client
-            Console.WriteLine("[info] Creating discord client..");
-            _discord = new DiscordClient(new DiscordConfiguration
-            {
-                Token = _config.GetValue<string>("discord:token"),
-                TokenType = TokenType.Bot
-            });
-
-            // Create the interactivity module(I'll show you how to use this later on)
-            _interactivity = _discord.UseInteractivity(new InteractivityConfiguration()
-            {
-                PaginationBehaviour = TimeoutBehaviour.Delete, // What to do when a pagination request times out
-                PaginationTimeout = TimeSpan.FromSeconds(30), // How long to wait before timing out
-                Timeout = TimeSpan.FromSeconds(30) // Default time to wait for interactive commands like waiting for a message or a reaction
-            });
-
-            // Build dependancies and then create the commands module.
-            var deps = BuildDeps();
-            _commands = _discord.UseCommandsNext(new CommandsNextConfiguration
-            {
-                StringPrefix = _config.GetValue<string>("discord:CommandPrefix"), // Load the command prefix(what comes before the command, eg "!" or "/") from our config file
-                Dependencies = deps // Pass the dependancies
-            });
-
-            // TODO: Add command loading!
-
-            RunAsync(args).Wait();
+            await _client.LoginAsync(TokenType.Bot, _config.GetValue<string>("discord:token"));
+            Console.WriteLine("Bot is ready !");
         }
         catch(Exception ex)
         {
@@ -79,29 +42,6 @@ internal class Program
 
      async Task RunAsync(string[] args)
     {
-        // Connect to discord's service
-        Console.WriteLine("Connecting..");
-        await _discord.ConnectAsync();
-        Console.WriteLine("Connected!");
-
-        // Keep the bot running until the cancellation token requests we stop
-        while (!_cts.IsCancellationRequested)
-            await Task.Delay(TimeSpan.FromMinutes(1));
+        return;
     }
-
-    /* 
-     DSharpPlus has dependancy injection for commands, this builds a list of dependancies. 
-     We can then access these in our command modules.
-    */
-    private DependencyCollection BuildDeps()
-        {
-            using var deps = new DependencyCollectionBuilder();
-
-            deps.AddInstance(_interactivity) // Add interactivity
-                .AddInstance(_cts) // Add the cancellation token
-                .AddInstance(_config) // Add our config
-                .AddInstance(_discord); // Add the discord client
-
-            return deps.Build();
-        }
 }
